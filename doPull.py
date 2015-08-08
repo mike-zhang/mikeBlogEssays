48152daf774caf576c6783edcd34282c4f2008b9
Mike_Zhang <Mike_Zhang@live.com>
2015-08-08 11:14:38 +0800

    添加代码同步脚本

---
+++ doPull.py
@@ -0,0 +1,8 @@
+﻿#! /usr/bin/env python
+#-*- coding: utf-8 -*-
+
+import os
+
+strCmd = "git pull"
+os.system(strCmd)
+
---
+++ doPush.py
@@ -0,0 +1,21 @@
+﻿#! /usr/bin/env python
+#-*- coding: utf-8 -*-
+
+import os
+
+def doGitPush():
+    bNameAll = os.popen("git branch -l").read()
+    #print bNameAll
+    bName = ""
+    for item in bNameAll.split('\n'):            
+        if item[0] == '*' :
+            bName = item
+            break
+    #print bName
+    bName = bName.split()[-1]
+    print bName
+    strCmd = "git push origin %s" % bName
+    print strCmd
+    os.system(strCmd)
+
+doGitPush()
